aec46f18025388427fef11c30fa169015e607bad
TimWe <tim.weiskopf@gmx.de>
2024-06-28 16:07:43 +0200

    💄

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,122 @@
-import Image from "next/image";
+import { cn } from '@/util/cn'
+import { Clock } from 'lucide-react'
+import { Fragment } from 'react'
 
 export default function Home() {
+  const amountPlaces = 20
+
+  const amountHours = 12
+  const startHour = 7
+
+  const hours = Array.from({ length: amountHours }, (_, i) => i + startHour)
+  const minuteSteps = [0, 15, 30, 45]
+
+  const timeStamps = hours.flatMap((hour) => {
+    return minuteSteps.map((minuteStep, idx) => {
+      return {
+        label: `${hour.toString().padStart(2, '0')}:${minuteStep
+          .toString()
+          .padStart(2, '0')}`,
+        isNewHour: idx === 0,
+      }
+    })
+  })
+  const topEntries = [...Array(amountPlaces)].map((_, i) => `Ort ${i + 1}`)
+
+  const appointments = [
+    {
+      startTime: '08:00',
+      duration: 4,
+      place: topEntries[0],
+      label: 'Test Appointment',
+    },
+  ]
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className=''>
+      <>
+        <div className='h-[100svh] overflow-scroll'>
+          <div className='flex w-min'>
+            <div
+              className='sticky left-0 z-50 grid bg-slate-100'
+              style={{
+                gridTemplateColumns: `56px`,
+              }}
+            >
+              <div className='sticky top-0 z-50 flex h-10 items-center justify-center self-start border-b border-r border-dotted bg-slate-100'>
+                <Clock className='h-5 w-5' />
+              </div>
+              {timeStamps.map((timeStamp, i) => {
+                return (
+                  <div
+                    key={i}
+                    className={cn(
+                      'sticky top-10 bg-slate-100 flex h-10 w-14 items-center justify-center border-b border-r border-dotted border-neutral-300',
+                      timeStamp.isNewHour && 'font-bold'
+                    )}
+                  >
+                    {timeStamp.label}
+                  </div>
+                )
+              })}
+            </div>
+            <div
+              className='grid flex-1'
+              style={{
+                gridTemplateColumns: `repeat(${topEntries.length}, minmax(175px, 1fr))`,
+              }}
+            >
+              {topEntries.map((label, i) => {
+                return (
+                  <div
+                    key={i}
+                    className='sticky top-0 left-14 z-40 flex h-10 flex-1 items-center gap-1 self-start truncate border-b border-r border-dotted bg-slate-100 px-2'
+                  >
+                    <div className='truncate sticky left-0'>{label}</div>
+                  </div>
+                )
+              })}
+              {timeStamps.map((timeStamp, i) => {
+                return (
+                  <Fragment key={i}>
+                    {topEntries.map((label, i2) => {
+                      const appointment = appointments.find((app) => {
+                        return (
+                          app.place === label &&
+                          timeStamp.label === app.startTime
+                        )
+                      })
+                      if (appointment) {
+                        return (
+                          <div className='h-10' key={i2}>
+                            <div
+                              className='flex justify-center items-center relative z-30 overflow-auto rounded-lg border-b border-dotted border-neutral-400  p-2 shadow-md shadow-neutral-400'
+                              style={{
+                                backgroundColor: 'lime',
+                                //this number needs to fit the height of left entries. currently h-10 = 40px
+                                height: 40 * appointment.duration + 'px',
+                              }}
+                            >
+                              {appointment.label}
+                            </div>
+                          </div>
+                        )
+                      } else {
+                        return (
+                          <div
+                            className='flex items-center justify-center h-10 border-b border-dotted border-neutral-300 font-extralight text-gray-400'
+                            key={i2}
+                          >{`${timeStamp.label} - ${label}`}</div>
+                        )
+                      }
+                    })}
+                  </Fragment>
+                )
+              })}
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </>
     </main>
-  );
+  )
 }
